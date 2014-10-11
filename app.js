f1ec165cd1ccd7be7b2f3a07092a3c691be1a308
Iszak Bryan <iszak.bryan@gmail.com>
2014-10-11 16:19:08 +0100

    Removed views

--- app.js
+++ app.js
@@ -9,10 +9,6 @@ var routes = require('./routes/index');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -23,7 +19,8 @@ app.use('/', routes);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
-    err.status = 404;
+        err.status = 404;
+
     next(err);
 });
 
